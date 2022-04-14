7a0094b728a6a5079e2466881222906c9629a523
CodeSik <CodeSik@users.noreply.github.com>
2022-04-14 06:35:28 +0000

    deployed via Gatsby Publish Action 🎩 for 9572338bfa2e1768f70840fd4a0c1ada4140da68

---
+++ component---src-pages-404-js-9db78011528d77a0c1a7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkCodeSik=self.webpackChunkCodeSik||[]).push([[883],{4393:function(e,t,n){n.r(t);var u=n(7294),l=n(4985),a=n(579);t.default=function(){return u.createElement(l.Z,null,u.createElement(a.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-9db78011528d77a0c1a7.js.map
